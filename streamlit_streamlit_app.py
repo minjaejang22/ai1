8c99fe1aa200813db6b5445d8bbbcc6f20ca5ebb
minjaejang22 <H098S02420@chunchon.gwe.hs.kr>
2025-11-26 09:14:36 +0900

    Update streamlit_streamlit_app.py

--- streamlit_streamlit_app.py
+++ streamlit_streamlit_app.py
@@ -74,8 +74,12 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
-}
 
+    labels[0] : {"texts" :["중국식 냉먄은 맛있어"], "images" : ["https://www.esquirekorea.co.kr/resources_old/online/org_online_image/eq/71c93efd-352d-4fb4-8a98-dd1b51475442.jpg"]},
+    labels[1] : {"texts" :["짜장면은 맛있어"], "images" : ["https://img.bizthenaum.co.kr/img2022/uyang_jjajang_01.jpg"]},
+    labels[2] : {"texts" :["짬뽕은 맛있어"], "images" : ["https://img-cf.kurly.com/hdims/resize/%3E720x/quality/90/src/shop/data/goodsview/20230803/gv20000714335_1.jpg"]},
+    labels[3] : {"texts" :["탕수 맛있어"], "images" : ["https://i.namu.wiki/i/DQPzlCdDvbadZasufp6VhKSLDPHU4YlA1f9GbY2fP9nMvjNOSE32JZxL_ysj2duhcUtcWo-cGQvO_qS3rXctJw.webp"]},  
+}   
 # ======================
 # 유틸
 # ======================
